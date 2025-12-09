1b21f48ee41f268275349c22a635190a23e3f076
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-09 17:08:49 +0500

    router file updated in modules package

--- modules/router.py
+++ modules/router.py
@@ -1,4 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from .scheme import PCAIn, PCAOut
+from PCA import PCA
 
+
+pca = PCA(2)
 
 modules_router = APIRouter()
+
+
+@modules_router.post(
+    '/',
+    summary='PCA fit transform',
+    status_code=status.HTTP_200_OK,
+    response_model=PCAOut
+)
+async def pca_fit(
+        pca_scheme: PCAIn
+) -> PCAOut:
+    pca.n_components = 2
+    pca_scheme = PCAOut(
+        X=pca.fit(pca_scheme.X)
+    )
+    return pca_scheme
