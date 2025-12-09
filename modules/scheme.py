6eefcf4d3c339e733df02e0d39913de88a66f84c
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-12-09 17:03:30 +0500

    scheme file updated in modules package

--- modules/scheme.py
+++ modules/scheme.py
@@ -1,9 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
+from numpy import array, isnan, where, nanmean, take
+from fastapi import HTTPException, status
 
 
 class PCAOut(BaseModel):
-    pass
 
+    X: list[list]
 
 class PCAIn(BaseModel):
-    pass
+    model_config = {
+        'extra': 'forbid'
+    }
+
+    X: list[list]
+
+    @model_validator('X')
+    def verify_X(cls, value):
+        X = array(value)
+        if X.ndim != 2:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+                detail='X must be 2D matrix'
+            )
+        col_mean = nanmean(X, axis=0)
+        inds = where(isnan(X))
+        X[inds] = take(col_mean, inds[1])
+        return X
